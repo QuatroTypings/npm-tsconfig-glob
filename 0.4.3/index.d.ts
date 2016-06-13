802f1237c303039eaa06b9c1098a3386025423ef
Giedrius Grabauskas <giedrius@quatrodev.com>
2016-06-13 12:00:46 +0300

    Removed contracts file and all context moved to index.d.ts.  … Updated test file.

--- 0.4.3/index.d.ts
+++ 0.4.3/index.d.ts
@@ -2,6 +2,46 @@
 // Project: https://github.com/wjohnsto/tsconfig-glob
 // Definitions by: Giedrius Grabauskas <https://github.com/GiedriusGrabauskas>
 
-import * as tsconfigGlob from './contracts';
-declare var _tsconfigGlob: tsconfigGlob.tsConfig;
-export = _tsconfigGlob;
+declare namespace tsconfigGlob {
+    interface Options {
+        /**
+         * A relative path from cwd to the directory containing a tsconfig.json. If not specified, the '.' is used.
+         */
+        configPath?: string;
+        /**
+         * Typescript confiuration file name in `configPath` directory
+         */
+        configFileName?: string;
+ 
+        /**
+         * The current working directory, defaults to `process.cwd()`
+         */
+        cwd?: string;
+ 
+        /**
+         * The number of spaces to indent the tsconfig.json
+         */
+        indent?: number;
+ 
+        /**
+         * Output an empty files array (ignoring the specified globs)
+         */
+        empty?: boolean;
+        
+        /**
+         * Asynchronous callback (default: true)
+         */
+        async?: boolean;
+    }
+
+    interface CallBack {
+        (err: any): void;
+    }
+
+    export interface tsConfig {
+        (options: Options, Function: CallBack): void;
+    }
+}
+
+declare var tsconfigGlob: tsconfigGlob.tsConfig;
+export = tsconfigGlob;
